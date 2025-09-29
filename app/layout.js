58d0da248461777154cf517a2ef1ad149632e306
baucucu <alexandru.raduca@gmail.com>
2025-09-29 09:56:13 +0300

    Refactor layout.js to consolidate favicon links into metadata for improved organization and maintainability. Remove deprecated Head component usage.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
-import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 import "./globals.css";
 
@@ -17,17 +16,19 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Endurance Ro-Index",
   description: "Prima platformă de date din România despre participarea și participanții la concursuri de masă - alergare, ciclism, triatlon.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -47,7 +48,6 @@ export default function RootLayout({ children }) {
             });
           `}
         </Script>
-        <NextScript />
         <Analytics />
       </body>
     </html>
