b2fbf0518b91b604c26ddd06c56c0ac6d971c69d
baucucu <alexandru.raduca@gmail.com>
2025-10-09 10:11:26 +0300

    Refactor SEO components by updating structured data in StructuredData.js, enhancing metadata in layout.js, and optimizing next.config.mjs with new custom headers for better search engine performance and visibility.

--- app/layout.js
+++ app/layout.js
@@ -114,7 +114,7 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon2.ico" type="image/x-icon" />
         <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
