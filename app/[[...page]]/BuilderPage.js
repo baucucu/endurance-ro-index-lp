3ec071c22802d6def080ac644ef436d51630f1ca
baucucu <alexandru.raduca@gmail.com>
2025-12-12 14:44:58 +0200

    Add empty lines to BuilderPage and BlogArticlePreviewPage components for improved readability and consistency across files.

--- app/[[...page]]/BuilderPage.js
+++ app/[[...page]]/BuilderPage.js
@@ -21,3 +21,4 @@ export default function BuilderPage({ content }) {
   return null;
 }
 
+
--- app/[[...page]]/page.js
+++ app/[[...page]]/page.js
@@ -87,3 +87,4 @@ export default async function CatchAllPage({ params }) {
   return <BuilderPage content={page} />;
 }
 
+
